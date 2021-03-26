75a04340d540c3f6845f306e8635c21e22bfc4e7
alanuq <alan.brown1@uqconnect.edu.au>
2021-03-26 10:38:56 +1000

    app.xaml initial commit

--- KRFC/KRFC/App.xaml.cs
+++ KRFC/KRFC/App.xaml.cs
@@ -9,8 +9,7 @@ namespace KRFC
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
